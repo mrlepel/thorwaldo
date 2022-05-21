c0856b7cfb988e65bce529ce46aba2bf9cca893f
riemer <70262649+mrlepel@users.noreply.github.com>
2022-05-21 11:40:29 +0200

    mirror

--- Assets/scripts/player_script.cs
+++ Assets/scripts/player_script.cs
@@ -3,6 +3,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_script : MonoBehaviour
+public class player_script : NetworkBehaviour
 {
+    [SyncVar]
+    public string Name = "";
+    [SyncVar]
+    public string Role = "";
+    
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
 }
